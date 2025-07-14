6e9bfa7c37b218fdc6365e2680fb53c8b525b39a
gzm-lab <gabriel@zibi.fr>
2025-07-14 02:09:45 +0200

    passage en prod

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Configuration CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Frontend URLs
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://calorie.gzi.ovh"],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
