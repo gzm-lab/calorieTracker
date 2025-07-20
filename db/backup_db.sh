8efd21987507fd351826d9ec4090aab935c9299b
Gabriel Zibi-Meyer <gabriel@zibi.fr>
2025-07-20 16:12:08 +0200

    Save db fix bug

--- db/backup_db.sh
+++ db/backup_db.sh
@@ -7,11 +7,10 @@ set +a
 
 BACKUP_DIR="/home/gzi/Desktop/calorie/backup"
 CSV_FILE="$BACKUP_DIR/calorietrack_backup.csv"
-TABLE="meals"
-CONTAINER="A_REMPLACER_PAR_LE_NOM_DU_CONTENEUR"  # À remplacer par le nom exact de ton conteneur PostgreSQL
+TABLE="meal"
 
 mkdir -p "$BACKUP_DIR"
 
 echo "Export de la table $TABLE en CSV..."
-docker exec -t $CONTAINER psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\copy $TABLE TO STDOUT WITH CSV HEADER" > "$CSV_FILE"
+docker exec -t "$POSTGRES_CONTAINER" psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\copy $TABLE TO STDOUT WITH CSV HEADER" > "$CSV_FILE"
 echo "Backup terminé : $CSV_FILE" 
